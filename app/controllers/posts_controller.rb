dc6ec3c4fcbe06516804b5440d8bc96b06bc14ea
Hunter <fleakhunter@gmail.com>
2021-03-03 14:37:43 -0500

    Create new and add post routes; update config.ru for Posts Controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+    get '/posts/new' do
+        if logged_in?
+            erb :'posts/new_post'
+        else 
+            redirect '/login'
+        end
+    end
+
+    post '/posts/add' do
+        if logged_in?
+            if params[:title].empty? || params[:body].empty?
+                @error = "Invalid.  The post title or body was empty."
+            else
+                #create the post
+                Post.create(title: params[:title], body: params[:body], jedi_id: session[:jedi_id])
+                redirect '/index'
+            end
+        else 
+            redirect '/login'
+        end
+    end
 end
