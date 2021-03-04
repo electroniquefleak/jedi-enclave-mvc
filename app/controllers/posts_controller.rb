80467fccfef277c31ffa4cf4695854474b6404f5
Hunter <fleakhunter@gmail.com>
2021-03-04 16:38:46 -0500

    Create further CRUD functionality with posts; show view for posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,48 @@ class PostsController < ApplicationController
             else
                 #create the post
                 Post.create(title: params[:title], body: params[:body], jedi_id: session[:jedi_id])
+                flash[message] = "You've come to the dark side. Post made."
                 redirect '/index'
             end
         else 
             redirect '/login'
         end
+    end
+
+    get '/posts/edit/:id' do
+        #verify/authenticate the current user & the post owner
+        @post = Post.find(params[:id])
+        if logged_in? && @post.jedi_id == current_user.id
+            @post
+            erb :'posts/edit_post'
+        else
+            # possible error or flash message
+            redirect '/index'
+        end 
+
+    end
+
+    patch "/posts/edit/:id" do
+        @post = Post.find(params[:id])
+        if logged_in? && @post.jedi_id == current_user.id
+            if params[:title].empty? || params[:body].empty?
+                flash[:message] = "Empty are your inputs!"
+                #flash[:alert_type] = danger
+                erb :'posts/edit_post'
+            else
+                Post.update(title: params[:title], body: params[:body])
+                flash[:message] = "Success! Updated is your post!"
+                #flash
+                redirect '/index'
+            end
+        else 
+            flash[:message] = "Incomplete are your credentials!"
+            redirect '/index'
+        end
+    end
+    #SHOW page
+    get '/posts/:id' do
+        @post = Post.find(params[:id])
+        erb :'posts/show_post'
     end
 end
