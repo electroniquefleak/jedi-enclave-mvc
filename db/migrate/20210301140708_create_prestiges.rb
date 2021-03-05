db64b7cce99e6f932160c8a9b7e9355680f40c63
Hunter <fleakhunter@gmail.com>
2021-03-05 12:52:15 -0500

    Refactor migration; create empty seed file.

--- db/migrate/20210301140708_create_prestiges.rb
+++ db/migrate/20210301140708_create_prestiges.rb
@@ -2,8 +2,9 @@ class CreatePrestiges < ActiveRecord::Migration[5.2]
   def change
     create_table :prestiges do |t|
       t.string :name
-      t.string :founder
       t.string :leader
+      t.string :description
+      t.string :image_url
 
       t.timestamps null: false
     end
