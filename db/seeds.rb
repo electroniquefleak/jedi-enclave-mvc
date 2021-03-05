17ad2d6179df9f813f73f1dbde664fe5ef77166a
Hunter <fleakhunter@gmail.com>
2021-03-05 13:29:45 -0500

    Create seed data for prestige classes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,28 @@
 require_relative '../config/environment'
+
+def create_all_prestiges
+    prestige_data = [
+        {
+            name: "Jedi Guardian",
+            leader: "Skarch Vaunk",
+            description: "Lorem ipsum",
+            image_url: "/images/Guardian_Protector.png"
+        },
+        {
+            name: "Jedi Sentinel",
+            leader: "Restelly Quist",
+            description: "Lorem ipsum",
+            image_url: "/images/Jedi_Sentinel_Shadow.png"
+        },
+        {   name: "Jedi Consular",
+            leader: "Sabla-Mandibu",
+            description: "Lorem ipsum",
+            image_url: "/images/Consular_Sage.png"
+        }
+    ]
+    prestige_data.each do |prestige|
+        Prestige.create(prestige)
+    end
+end
+
+create_all_prestiges
